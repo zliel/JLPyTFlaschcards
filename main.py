fb82674d830d9effa43921e575251e077b2d20f6
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-07-12 16:18:53 -0700

    Added a basic home page to the GUI

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+
+# noinspection PyUnresolvedReferences
+from __feature__ import snake_case, true_property
+
+
+my_app = QApplication([])
+
+
+class MainWindow(QWidget):
+    """This class defines the main window of the application, which will house all other necessary widgets."""
+    def __init__(self):
+        super().__init__()
+        self.layout = QVBoxLayout()
+        self.layout.add_widget(QLabel("Welcome to JLPyTFlashcards, your hub for practicing your JLPT Vocabulary"))
+        self.set_layout(self.layout)
+
+        self.window_title = "JLPyT Flashcards"
+        self.resize(1200, 700)
+        self.palette = Qt.black
+
+
+main_window = MainWindow()
+main_window.show()
+
+sys.exit(my_app.exec())
