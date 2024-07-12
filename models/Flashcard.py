df22c724adeaf7787afe1f1c35edd84281e63e94
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-07-12 16:30:28 -0700

    Added dunder methods to the flashcard model

--- models/Flashcard.py
+++ models/Flashcard.py
@@ -6,3 +6,27 @@ class Flashcard:
         self.question = question
         self.answer = answer
         self.next_review_date = datetime.now()
+
+    def __str__(self):
+        return f"Question: {self.question}\nAnswer: {self.answer}\nNext Review Date: {self.next_review_date}"
+
+    def __eq__(self, other):
+        return self.question == other.question and self.answer == other.answer
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __lt__(self, other):
+        return self.next_review_date < other.next_review_date
+
+    def __le__(self, other):
+        return self.next_review_date <= other.next_review_date
+
+    def __gt__(self, other):
+        return self.next_review_date > other.next_review_date
+
+    def __ge__(self, other):
+        return self.next_review_date >= other.next_review_date
+
+    def __repr__(self):
+        return f"Flashcard({self.question}, {self.answer}, {self.next_review_date})"
