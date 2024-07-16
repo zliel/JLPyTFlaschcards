a5de7f9c4bf46e65903fdbc0ea34682642f5930e
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-07-15 23:47:41 -0700

    Added a method to add new flashcards to a selected deck

--- widgets/AddCardWidget.py
+++ widgets/AddCardWidget.py
@@ -4,6 +4,8 @@ from PySide6.QtCore import Qt, Slot
 # noinspection PyUnresolvedReference
 from __feature__ import snake_case, true_property
 
+from models.Flashcard import Flashcard
+
 
 class AddCardWidget(QWidget):
     def __init__(self, app_decks):
@@ -28,9 +30,21 @@ class AddCardWidget(QWidget):
         self.layout.add_widget(self.answer_input)
 
         self.add_card_button = QPushButton("Add Card")
-        # self.add_card_button.clicked.connect(self.add_card)
+        self.add_card_button.clicked.connect(self.add_card)
         self.layout.add_widget(self.add_card_button)
 
         self.set_layout(self.layout)
         self.resize(400, 300)
         self.show()
+
+    @Slot()
+    def add_card(self):
+        deck_name = self.deck_dropdown.current_text
+        question = self.question_input.text
+        answer = self.answer_input.text
+
+        for deck in self.decks:
+            if deck.name == deck_name:
+                deck.append_card(Flashcard(question, answer))
+                break
+        self.close()
