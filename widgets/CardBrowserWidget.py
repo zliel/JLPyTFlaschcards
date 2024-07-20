d86c2ff61685e157a38421f9fba188f53623c6d3
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-07-19 22:19:39 -0700

    Added all cards to the browser by default and a way to show the card editor

--- widgets/CardBrowserWidget.py
+++ widgets/CardBrowserWidget.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QLabel, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QListWidget
+from PySide6.QtWidgets import QLabel, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QListWidget, QListWidgetItem
 from PySide6.QtCore import Qt, Slot
 
 # noinspection PyUnresolvedReference
@@ -18,8 +18,24 @@ class CardBrowserWidget(QWidget):
         self.layout.add_widget(self.filter_list_widget)
 
         self.card_list_widget = QListWidget()
+        for deck in app_decks:
+            for card in deck.cards:
+                item = QListWidgetItem(f"{card.question} : {card.answer}")
+                item.set_data(Qt.UserRole, card)
+                self.card_list_widget.add_item(item)
+        self.card_list_widget.itemDoubleClicked.connect(lambda item: self.show_card_editor(item))
         self.layout.add_widget(self.card_list_widget)
 
-        self.card_edit_widget = None
+        self.card_edit_widget = CardEditWidget()
+        # self.layout.add_widget(self.card_edit_widget)
 
+        self.set_layout(self.layout)
         self.show()
+
+    @Slot()
+    def show_card_editor(self, item):
+        card = item.data(Qt.UserRole)
+        if self.card_edit_widget:
+            self.card_edit_widget.close()
+        self.card_edit_widget = CardEditWidget(card)
+        self.layout.add_widget(self.card_edit_widget)
