5af4b2235f580f509483efc0341401476b4af77a
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-07-19 22:19:08 -0700

    Updated the layout to allow for dynamically changing cards

--- widgets/CardEditWidget.py
+++ widgets/CardEditWidget.py
@@ -4,31 +4,41 @@ from PySide6.QtCore import Qt, Slot
 # noinspection PyUnresolvedReference
 from __feature__ import snake_case, true_property
 
+from models.Flashcard import Flashcard
+
 
 class CardEditWidget(QWidget):
-    def __init__(self, card):
+    def __init__(self, card: Flashcard = None) -> None:
         super().__init__()
         self.layout = QVBoxLayout()
+        self.card = card
 
         self.front_label = QLabel("Front:")
         self.layout.add_widget(self.front_label)
         self.front_input = QLineEdit()
-        self.front_input.text = card.front
         self.layout.add_widget(self.front_input)
 
         self.back_label = QLabel("Back:")
         self.layout.add_widget(self.back_label)
         self.back_input = QLineEdit()
-        self.back_input.text = card.back
         self.layout.add_widget(self.back_input)
 
         self.tags_label = QLabel("Tags (seperate by spaces):")
         self.layout.add_widget(self.tags_label)
         self.tags_input = QLineEdit()
-        self.tags_input.text = " ".join(card.tags)
         self.layout.add_widget(self.tags_input)
 
         self.save_button = QPushButton("Save")
         self.layout.add_widget(self.save_button)
 
-        self.show()
+        self.set_layout(self.layout)
+
+        if self.card:
+            self.load_card()
+        # self.show()
+
+    def load_card(self):
+        self.front_input.text = self.card.question
+        self.back_input.text = self.card.answer
+        # example: "tag1 tag2 tag3"
+        self.tags_input.text = " ".join(self.card.tags)
