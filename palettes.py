b3312acc01e060557b090c3f8aea6bb604b76971
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-07-23 20:41:18 -0700

    Added a font for buttons

--- palettes.py
+++ palettes.py
@@ -6,6 +6,7 @@ from __feature__ import snake_case, true_property
 large_label_font = QFont("Times New Roman", 18)
 default_text_font = QFont("Times New Roman", 12)
 list_item_font = QFont("Times New Roman", 14)
+button_font = QFont("Times New Roman", 12)
 
 palette = {
     'primary_100': QColor('#76c4e8'),
