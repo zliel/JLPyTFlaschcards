15f3c2d267b30af9869eea918e7fe0c98de19d89
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-07-31 18:20:36 -0700

    Added a Factory class to generate themes based on the user's settings

--- theme.py
+++ theme.py
@@ -30,23 +30,55 @@ palette = {
     'fail': QColor('#ff599c'),
 }
 
-role_to_color_map = {
-    QPalette.ColorRole.Window: palette['dark_100'],
-    QPalette.ColorRole.WindowText: palette['text'],
-    QPalette.ColorRole.Base: palette['dark_200'],
-    QPalette.ColorRole.AlternateBase: palette['dark_300'],
-    QPalette.ColorRole.ToolTipBase: palette['dark_300'],
-    QPalette.ColorRole.ToolTipText: palette['text'],
-    QPalette.ColorRole.Text: palette['text'],
-    QPalette.ColorRole.Button: palette['dark_200'],
-    QPalette.ColorRole.ButtonText: palette['primary_500'],
-    QPalette.ColorRole.BrightText: palette['primary_500'],
-    QPalette.ColorRole.Highlight: palette['highlight'],
-    QPalette.ColorRole.HighlightedText: palette['text'],
-    QPalette.ColorRole.Accent: palette['primary_200'],
-}
+# Change "dark" to "background" and have multiple palettes
 
-blue_dark_palette = QPalette()
+class PaletteFactory:
 
-for role, color in role_to_color_map.items():
-    blue_dark_palette.set_color(role, color)
+    @staticmethod
+    def create_palette(theme_name: str) -> QPalette:
+
+        # blue, green, purple, pink
+        palettes = {
+            'dark_blue': {
+                'primary_100': QColor('#76c4e8'),
+                'primary_200': QColor('#88caeb'),
+                'primary_300': QColor('#98d1ed'),
+                'primary_400': QColor('#a8d8f0'),
+                'primary_500': QColor('#b7def2'),
+                'primary_600': QColor('#c6e5f5'),
+                'background_100': QColor('#121212'),
+                'background_200': QColor('#282828'),
+                'background_300': QColor('#3f3f3f'),
+                'background_400': QColor('#575757'),
+                'background_500': QColor('#717171'),
+                'background_600': QColor('#8b8b8b'),
+                'text': QColor('#e8eaf6'),
+                'highlight': QColor('#0a74a6'),
+                'pass': QColor('#23ebcd'),
+                'fail': QColor('#ff599c')
+            }
+        }
+
+        new_palette = QPalette()
+        palette_to_use = palettes[theme_name]
+
+        role_to_color_map = {
+            QPalette.ColorRole.Window: palette_to_use['background_100'],
+            QPalette.ColorRole.WindowText: palette_to_use['text'],
+            QPalette.ColorRole.Base: palette_to_use['background_200'],
+            QPalette.ColorRole.AlternateBase: palette_to_use['background_300'],
+            QPalette.ColorRole.ToolTipBase: palette_to_use['background_300'],
+            QPalette.ColorRole.ToolTipText: palette_to_use['text'],
+            QPalette.ColorRole.Text: palette_to_use['text'],
+            QPalette.ColorRole.Button: palette_to_use['background_200'],
+            QPalette.ColorRole.ButtonText: palette_to_use['primary_500'],
+            QPalette.ColorRole.BrightText: palette_to_use['primary_500'],
+            QPalette.ColorRole.Highlight: palette_to_use['highlight'],
+            QPalette.ColorRole.HighlightedText: palette_to_use['text'],
+            QPalette.ColorRole.Accent: palette_to_use['primary_200'],
+        }
+
+        for role, color in role_to_color_map.items():
+            new_palette.set_color(role, color)
+
+        return new_palette
